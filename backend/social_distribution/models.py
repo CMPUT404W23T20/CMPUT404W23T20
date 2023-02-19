a242a39f02f893036b29c5b9fd119d65916b396a
bdirksen <97187485+bdirksen@users.noreply.github.com>
2023-02-18 19:44:31 -0700

    removed unneeded ids

--- backend/social_distribution/models.py
+++ backend/social_distribution/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
-    id = models.CharField(max_length=200, primary_key=True)
     host = models.CharField(max_length=200)
     displayName = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
@@ -14,8 +13,7 @@ class Author(models.Model):
         return self.displayName
 
 class Comment(models.Model):
-    id = models.CharField(max_length=200, primary_key=True)
-    auther = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     comment = models.CharField(max_length=200)
     contentType = models.CharField(max_length=200)
     published = models.DateTimeField()
@@ -25,15 +23,14 @@ class Comment(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    id = models.CharField(max_length=200, primary_key=True)
     source = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     contentType = models.CharField(max_length=200)
-    auther = models.ForeignKey(Author, on_delete=models.CASCADE)
+    #author = models.ForeignKey(Author, on_delete=models.CASCADE)
     categories = models.CharField(max_length=200)
     count = models.IntegerField()
     comments = models.CharField(max_length=200)
-    commentSrc = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    #commentSrc = models.ForeignKey(Comment, on_delete=models.CASCADE)
     published = models.DateTimeField()
     visibility = models.CharField(max_length=200)
     unlisted = models.BooleanField()
