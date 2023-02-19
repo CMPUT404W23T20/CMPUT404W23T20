3049461cf901332efaf4dc6c1d1c7bf64c575107
bdirksen <97187485+bdirksen@users.noreply.github.com>
2023-02-19 01:58:10 -0700

    added post view

--- frontend/src/Components/Posts.js
+++ frontend/src/Components/Posts.js
@@ -37,6 +37,8 @@ function Posts() {
 
 
     const [createPost, setcreatePost] = React.useState([false]);
+    const [openPost, setopenPost] = React.useState([false]);
+    const [post, setPost] = React.useState([{}]);
     return (
         <Box>
             <Box className="App" style={{ display: "flex", flexDirection: "row", height : "100vh", width: "100vw", alignItems: "left", justifyContent: "left" }}>
@@ -44,11 +46,11 @@ function Posts() {
                 <Nav />
                 </Box>
                 <Box style={{ display: "flex", flexDirection: "row", backgroundColor: "white", flex: 1, height: "100vh"}}>
-                    <Box style={{display: "flex", flexDirection: "column",flex: 1, margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "10px"}}>
+                    <Box style={{display: "flex", flexDirection: "column",flex: 1, margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "5px"}}>
                         <Typography variant="h4">Posts</Typography>
                         <List style = {{ flex: 1, overflowY: "scroll"}}>
                             {Posts.map((post) => (
-                                <ListItem key={post.id}>
+                                <ListItem key={post.id} onClick = {() => {setopenPost(true); setPost(post)}}>
                                     <Card style = {{ width: "100%", backgroundColor: "#66aeec"}}>
                                         <Box style = {{ paddingLeft: 2}}>
                                             <Typography variant="h5">{post.title}</Typography>
@@ -59,27 +61,49 @@ function Posts() {
                                 </ListItem>
                             ))}
                         </List>
+                        <Button variant="contained" color="primary" onClick={() => setcreatePost(true)} style = {{margin: 10, alignSelf: "flex-end"}}>
+                            Create Post
+                        </Button>
                     </Box>
+                    {openPost && (
+                        <Box style={{ flex: 1,display: "flex", flexDirection: "column", margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "5px"}}>
+                            <Typography variant="h4">{post.title}</Typography>
+                            <Typography variant="body2">{post.author}</Typography>
+                            <Box style={{flex: 1, margin: "5px"}}>
+                                <Card style = {{ width: "100%", height: "100%", borderRadius: "4px", boxShadow: "0 0 10px 0 rgba(0,0,0,0.5)"}}>
+                                    <Typography variant="body1">{post.description}</Typography>
+                                </Card>
+                            </Box>
+                            <Box style={{alignSelf: "flex-end"}}>
+                                <Button variant="contained" color="primary" onClick={() => setopenPost(false)} style = {{margin: 10, alignSelf: "flex-end"}}>
+                                    Update
+                                </Button>
+                                <Button variant="contained" color="secondary" onClick={() => setopenPost(false)} style = {{margin: 10, alignSelf: "flex-end"}}>
+                                    Close
+                                </Button>
+                            </Box>
+                        </Box>)
+                    }
                     {createPost && (
-                        <Box style={{ display: "flex", flexDirection: "column", margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "10px", width: "25%"}}>
+                        <Box style={{ display: "flex", flexDirection: "column", margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "5px", width: "25%"}}>
                             <Typography variant="h4">Create Post</Typography>
                             <Box style={{ display: "flex", flexDirection: "column", flex: 1, margin: "10px", alignItems: "center"}}>
                                 <TextField id="title" label="Title" variant="outlined" style={{width: "95%", margin: "25px"}}/>
                                 <TextField id="description" label="Description" variant="outlined" style={{width: "95%", margin: "25px"}} multiline minRows={20}/>
                             </Box>
+                            <Box style={{alignSelf: "flex-end"}}>
+                                <Button variant="contained" color="primary" onClick={() => CreatePost(document.getElementById("title").value, document.getElementById("description").value)} style = {{margin: 10, alignSelf: "flex-end"}}>
+                                    Create
+                                </Button>
+                                <Button variant="contained" color="secondary" onClick={() => setcreatePost(false)} style = {{margin: 10, alignSelf: "flex-end"}}>
+                                    Cancel
+                                </Button>
+                            </Box>
+                            
                         </Box>)
-                    }
+                    } 
                 </Box>
             </Box>
-            {/*create post button absolute position top right*/}
-            <Box style={{position: "absolute", bottom: "20px", right: "20px"}}>
-                <Button variant="contained" color="primary" onClick={createPost ? () => CreatePost(document.getElementById("title").value, document.getElementById("description").value) : () => setcreatePost(true)}>
-                    {createPost ? "Done" : "Create Post"}
-                </Button>
-                <Button variant="contained" color="secondary" onClick={() => setcreatePost(false)} style={{marginLeft: "10px"}}>
-                    Cancel
-                </Button>
-            </Box>  
         </Box>
     )
 }
