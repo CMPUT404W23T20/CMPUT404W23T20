74dfdd3f224a768f7bec5873f3a530f3c190a425
Krizzia Concepcion <krizzia@ualberta.ca>
2023-02-20 19:41:56 -0700

    UI to send friend request to local authors

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 #Resources
 https://medium.com/analytics-vidhya/add-friends-with-689a2fa4e41d
+
+https://reactgo.com/react-change-button-color-onclick/
--- frontend/src/Components/Friends.js
+++ frontend/src/Components/Friends.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Card,CardContent,List, ListItem, RadioGroup, TextField, Typography } from '@material-ui/core';
 import Nav from './Nav';
 import axios from 'axios';
@@ -17,29 +17,46 @@ function Friends() {
         });
     }, []);
 
+    //https://reactgo.com/react-change-button-color-onclick/
+    const [active, setActive] = useState(false);
+  
+    const handleClick = (x) => {
+      setActive(!active);
+
+        
+      
+      const btn = active ? document.getElementById(x).style.backgroundColor = "white": document.getElementById(x).style.backgroundColor = "lightgrey" ;
+      
+      var btnText= active ? document.getElementById(x).innerText = "Add Friend": document.getElementById(x).innerText= "Request Sent" ;
+      
+        
+    };
     
     return (
         <Box>
             <h1>Friends</h1>
     
-            <div style = {{float:"right",paddingRight:50,width: 400,}}>
-                <Card style={{ width: 400,height:400, backgroundColor:"#66aeec",overflowY:"scroll"}}>
+            <div style = {{float:"right",paddingRight:150,width: 400,}}>
+                <Card style={{ width: 450,height:450, backgroundColor:"#66aeec",overflowY:"scroll"}}>
                     
                         <h2 style ={{color:"whitesmoke"}}>Local Authors</h2>
-
+                        
                         {Users.map((Users) => (
-                             <CardContent>
+                             <CardContent >
                                 <div class = "LocalAuth" style = {{display:'flex',alignItems:'center',width:400,wordWrap:"break-word"}}>
                                      <img src= {Users.profileImage} alt = "Profile" style = {{borderRadius:"50%",marginRight:20}} width={55} height = {55}/>
                                      <span>
                                         <a href = " "><h4 style ={{width:150,wordWrap:"break-word"}}>{Users.displayName}  </h4></a>
                                      </span>
-                                     <Button style={{float:"right", backgroundColor:"whitesmoke",marginLeft:25}}>Add Friend</Button>
+                                     <Button id = {Users.id} onClick={() => handleClick(Users.id)}  
+                                        style={{backgroundColor: "white",float:"right",
+                                        marginLeft:25, fontSize:15}}>
+                                            Add Friend
+                                    </Button>
 
                                 </div>
                            
-                                
-                             
+
                             </CardContent>    
                          
                         ))}
