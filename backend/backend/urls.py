5cb7cd9455dfa32ce56f9330da4ab79e1fdfb930
bdirksen <97187485+bdirksen@users.noreply.github.com>
2023-03-10 20:16:52 -0700

    added documentation page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,15 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from social_distribution import views         
-
-router = routers.DefaultRouter()     
+from django.urls import path
+from social_distribution import views        
+from rest_framework.schemas import get_schema_view 
+from django.views.generic import TemplateView 
 
 
 
 urlpatterns = [
+    path('api_schema/', get_schema_view(
+        title='API Schema',
+        description='REST API'
+    ), name='api_schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url':'api_schema'}
+        ), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('login', views.LoginView.as_view(), name='login'),
     path('service/posts', views.posts, name='posts'),
