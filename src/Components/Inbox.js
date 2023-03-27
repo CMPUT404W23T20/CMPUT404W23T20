02a934ef4786f912c093f5fc453d46a40df9c187
bdirksen <97187485+bdirksen@users.noreply.github.com>
2023-03-27 13:08:18 -0600

    updated inbox look (#81)
    * updated inbox look
    
    * added build
    
    * fixed issue showing wrong profile picture on follows

--- src/Components/Inbox.js
+++ src/Components/Inbox.js
@@ -3,10 +3,13 @@ import { Box, Button, Card, List, ListItem, TextField, Typography } from '@mater
 import Nav from './Nav';
 import axios from 'axios';
 import { getApiUrls } from '../utils/utils';
+import CircularProgress from '@mui/material/CircularProgress';
 
 
 function Inbox() {
+    const [loading, setLoading] = React.useState(true);
     const get_inbox_items = async () => {
+        setLoading(true);
         let path = `${getApiUrls()}/service/authors/${localStorage.getItem("id")}/inbox`;
         let response = await axios.get(path, {
             headers: {
@@ -15,6 +18,7 @@ function Inbox() {
             }
         });
         console.log(response.data.items);
+        setLoading(false);
         return response.data.items
     }
 
@@ -53,15 +57,45 @@ function Inbox() {
         return response.data;
     }
 
-    const [Items, setItems] = React.useState([]);
+    const [items, setItems] = React.useState([]);
     React.useEffect(() => {
         get_inbox_items().then((data) => {
             setItems(data);
         });
     }, []);
 
-    const [openItem, setopenItem] = React.useState(false);
-    const [item, setItem] = React.useState([{}]);
+    const [comments, setComments] = React.useState([]);
+
+    const handleOpenPost = async (post) => {
+        // get all information on post
+        setComments([])
+        let path
+        let response
+        path = `${getApiUrls()}/service/authors/${post.author.id ? post.author.id : post.author}/posts/${post.id}`;
+        response = await axios.get(path, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        setPost(response.data);
+
+        // get comments on post
+        console.log (response.data.author.id, localStorage.getItem("id"))
+        if (response.data.author.id === localStorage.getItem("id")) {
+            path = `${getApiUrls()}/service/authors/${post.author.id}/posts/${post.id}/comments`;
+            response = await axios.get(path, {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+            setComments(response.data.items);
+        }
+        
+        setopenPost(true);
+    }
+
+    const [openPost, setopenPost] = React.useState(false);
+    const [post, setPost] = React.useState();
     return (
         <Box>
             <Box className="App" style={{ display: "flex", flexDirection: "row", height: "100vh", width: "100vw", alignItems: "left", justifyContent: "left" }}>
@@ -69,56 +103,53 @@ function Inbox() {
                     <Nav />
                 </Box>
                 <Box style={{ display: "flex", flexDirection: "row", backgroundColor: "white", flex: 1, height: "100vh" }}>
-                    <Box style={{ display: "flex", flexDirection: "column", flex: 1, margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "5px" }}>
+                    <Box style={{ display: "flex", flexDirection: "column", flex: 1, margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "5px", backgroundColor: "#c3d3eb" }}>
                         <Typography variant="h4">Inbox</Typography>
-                        <List style={{ flex: 1, overflowY: "scroll" }}>
-                            {Items.map((item) => (
-                                <ListItem key={item.id} onClick={() => { if (item.type === "post") { setItem(item); setopenItem(true); } }}>
-                                    <Card style={{ width: "100%", backgroundColor: "#8fd1f2" }}>
-                                        <Box style={{ paddingLeft: 2 }}>
-                                            {item.type === "post" && (
-                                                <Box style={{ margin: 10 }}>
-                                                    <Typography variant="h5">Post</Typography>
-                                                    <Typography variant="h6">{item.title}</Typography>
-                                                    <Typography variant="body2">{item.author.displayName}</Typography>
-                                                    <Typography variant="body1" style={{ maxHeight: "200px", overflowY: "auto" }}>{item.description}</Typography>
+                        <List style = {{ flex: 1, overflowY: "scroll", maxHeight: "100%"}}>
+                            {loading && <CircularProgress />}
+                            {!loading && items.map((item) => (
+                                <ListItem key={item.id}>
+                                    {item.type === "post" && (<Card style = {{ width: "100%"}} onClick = {() => {handleOpenPost(item)}}>
+                                        <Box style = {{ paddingLeft: 2}}>
+                                            {(item.type === 'post') && (<Box style = {{ display: "flex", flexDirection: "row", marginTop: "10px", marginLeft: "10px"}}>
+                                                <img src= {(item.author.profileImage != "no profileImage" && item.author.profileImage != "") ? item.author.profileImage : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2048px-Solid_white.svg.png"} alt = "IMG" style = {{borderRadius:"50%"}} width="100px" height = "100px"/>
+                                                <Box style = {{ display: "flex", flexDirection: "column", paddingLeft: "10px"}}>
+                                                    <Typography variant="h5">Title: {item.title}</Typography>
+                                                    <Typography variant="body2">Author: {item.author.displayName}</Typography>
+                                                    <Typography variant="body2">Published: {item.published.substring(0,10)}</Typography>
+                                                    <Typography variant="body2">Node: {item.author.host}</Typography>
                                                 </Box>
-
-                                            )}
-                                            {item.type === "comment" && (
-                                                <Box>
-                                                    <div style={{ display: 'flex', justifyContent: 'space-between', margin: 10 }}>
-                                                        <Typography variant="h5">Comment</Typography>
-                                                        <Typography variant="body2">{item.author.displayName}</Typography>
-                                                        <Typography variant="body1" style={{ maxHeight: "200px", overflowY: "auto" }}>{item.comment}</Typography>
-                                                    </div>
-                                                </Box>
-                                            )}
-                                            {item.type === "follow" && (
-                                                <Box>
-                                                    <div style={{ display: 'flex', justifyContent: 'space-between', margin: 10 }}>
-                                                        <span>
-                                                            <Typography variant="h5">Follow</Typography>
-                                                            <Typography variant="body2">{item.summary}</Typography>
-                                                        </span>
-                                                        <Button
-                                                            id={item.follower}
-                                                            style={{
-                                                                backgroundColor: "white", float: "right",
-                                                                marginLeft: 25, fontSize: 15, minWidth: 90
-                                                            }} onClick={() => followBack(item.follower)}>
-                                                            Follow Back
-                                                        </Button>
-                                                    </div>
-                                                </Box>
-                                            )}
-                                            {item.type === "like" && (
-                                                <Box>
-                                                    <Typography variant="h5">{item.author.displayName + " liked your post " + item.post.title}</Typography>
-                                                </Box>
-                                            )}
+                                            </Box>)}
                                         </Box>
-                                    </Card>
+                                    </Card>)}
+                                    {item.type === "comment" && (<Card style = {{ width: "100%"}} onClick = {() => {handleOpenPost(item.post)}}>
+                                        <Box style = {{ paddingLeft: 2}}>
+                                            <Box style = {{ display: "flex", flexDirection: "row", marginTop: "10px", marginLeft: "10px"}}>
+                                                <img src= {(item.author.profileImage != "no profileImage" && item.author.profileImage != "") ? item.author.profileImage : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2048px-Solid_white.svg.png"} alt = "IMG" style = {{borderRadius:"50%"}} width="100px" height = "100px"/>
+                                                <Box style = {{ display: "flex", flexDirection: "column", paddingLeft: "10px"}}>
+                                                    <Typography variant="h5">Comment: {item.comment}</Typography>
+                                                    <Typography variant="body2">Author: {item.author.displayName}</Typography>
+                                                    <Typography variant="body2">Published: {item.published.substring(0,10)}</Typography>
+                                                    <Typography variant="body2">Node: {item.author.host}</Typography>
+                                                </Box>
+                                            </Box>
+                                        </Box>
+                                    </Card>)}
+                                    {item.type === "follow" && (<Card style = {{ width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between"}} onClick = {() => {handleOpenPost(item.post)}}>
+                                        <Box style = {{ paddingLeft: 2}}>
+                                            <Box style = {{ display: "flex", flexDirection: "row", marginTop: "10px", marginLeft: "10px"}}>
+                                                <img src= {(item.follower.profileImage != "no profileImage" && item.follower.profileImage != "") ? item.follower.profileImage : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2048px-Solid_white.svg.png"} alt = "IMG" style = {{borderRadius:"50%"}} width="100px" height = "100px"/>
+                                                <Box style = {{ display: "flex", flexDirection: "column", paddingLeft: "10px"}}>
+                                                    <Typography variant="h5">Follow Request</Typography>
+                                                    <Typography variant="body2">Author: {item.follower.displayName}</Typography>
+                                                    <Typography variant="body2">Node: {item.follower.host}</Typography>
+                                                </Box>
+                                            </Box>
+                                        </Box>
+                                        <Button id={item.follower} style={{ margin: 42, alignSelf: "flex-end",backgroundColor: "lightblue" }} onClick={() => followBack(item.follower)} variant="contained">
+                                            Accept
+                                        </Button>
+                                    </Card>)}       
                                 </ListItem>
                             ))}
                         </List>
@@ -129,15 +160,39 @@ function Inbox() {
                             Refresh
                         </Button>
                     </Box>
-                    {openItem && (
-                        <Box style={{ flex: 1, display: "flex", flexDirection: "column", margin: "10px", borderColor: "grey", borderStyle: "solid", borderRadius: "5px" }}>
-                            <Box style={{ flex: 1, margin: "5px" }}>
-                                <Card style={{ width: "100%", height: "100%", borderRadius: "4px", boxShadow: "0 0 10px 0 rgba(0,0,0,0.5)" }}>
-                                    <TextField id="description" label="Description" variant="outlined" style={{ width: "95%", margin: "25px" }} value={item.description} onChange={(e) => setItem({ ...item, description: e.target.value })} multiline maxRows={15} />
-                                </Card>
-                            </Box>
-                        </Box>)
-                    }
+                    {openPost && (
+                        <Box style={{flex: 1, margin: "10px", borderColor: "grey", borderStyle: "solid", backgroundColor: "#c3d3eb", display: "flex", flexDirection: "column"}}>
+                            <Card style = {{ marginRight: "10px",marginBottom: "10px",marginLeft: "10px", borderRadius: "10px", borderColor: "black",marginTop: "5px",flex:1}}>
+                                <Typography variant="h2">{post.title}</Typography>
+                                <Box>
+                                    <img src= {(post.author.profileImage != "no profileImage" && post.author.profileImage != "") ? post.author.profileImage : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2048px-Solid_white.svg.png"} alt = "IMG" style = {{borderRadius:"50%"}} width="100px" height = "100px"/>
+                                    <Box style = {{ display: "flex", flexDirection: "column", paddingLeft: "10px", alignItems: "cen", justifyContent: "left"}}>
+                                        <Typography variant="body2">Author: {post.author.displayName}</Typography>
+                                        <Typography variant="body2">Published: {post.published.substring(0,10)}</Typography>
+                                        <Typography variant="body2">Node: {post.author.host}</Typography>
+                                    </Box>
+                                </Box>
+                                <Typography variant="h5">Description:</Typography>
+                                <Typography variant="body2">{post.description}</Typography>
+                                <div id = "postedComment" style = {{display:"none",borderRadius:"25px",backgroundColor:"#bce3c0",width: "40%", margin: "10px", paddingLeft:"5%",marginLeft:"30%"}}>
+                                </div>
+                                <Button variant="contained" color="secondary" onClick={() => setopenPost(false)} style={{ position: "absolute", bottom: "30px", right: "30px"}}>
+                                    Close
+                                </Button>
+                            </Card>
+                            <Card style = {{ marginRight: "10px",marginBottom: "10px",marginLeft: "10px", borderRadius: "10px", borderColor: "black",marginTop: "5px",flex:1, overflowY: "scroll"}}>
+                                <Typography variant="h6" style = {{textAlign:"left", paddingLeft:30,fontSize:20}}>Comments:</Typography>                                        
+                                {comments.map((comments) => (
+                                        <div style = {{display:'flex',alignItems:'center',wordWrap:"break-word"}}>
+                                            <img src= {comments.author.profileImage} alt = "" style = {{borderRadius:"50%",marginLeft:30,marginRight:15,marginBottom:10}} width={55} height = {55}/>
+                                            <Typography variant="h6" style = {{display: "inline-block",textAlign:"left", paddingLeft:15,fontSize:20}}>
+                                                {comments.author.displayName}: {comments.comment}
+                                            </Typography>
+                                        </div>
+                                    ))}
+                            </Card>
+                        </Box>
+                    )}
                 </Box>
             </Box>
         </Box>
