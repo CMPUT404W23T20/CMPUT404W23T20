d970308842a96ca767b6426583d217a83a4a45f6
bdirksen <97187485+bdirksen@users.noreply.github.com>
2023-03-26 22:51:52 -0600

    add all again

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'django-insecure-hw=1yrhm5yi61$z6!oo*jwb8$m@vv8ryfmnq=aoa+#-4(z7v$#
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '*'
+    'http://localhost:3000','http://127.0.0.1:8000','http://localhost:8000',
+    'https://t20-social-distribution.herokuapp.com','http://localhost:8001',
+    'https://social-distribution-media.herokuapp.com','https://epic-app.vercel.app',
+    'https://group-13-epic-app.herokuapp.com', 'https://social-distribution-media.herokuapp.com',
+    'https://cmput404-group6-instatonne.herokuapp.com', 'https://*', 'http://*'
 ]
 
 
@@ -77,11 +81,19 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
-    '*'
+    'http://localhost:3000','http://127.0.0.1:8000','http://localhost:8000',
+    'https://t20-social-distribution.herokuapp.com','http://localhost:8001',
+    'https://social-distribution-media.herokuapp.com','https://epic-app.vercel.app',
+    'https://group-13-epic-app.herokuapp.com', 'https://social-distribution-media.herokuapp.com',
+    'https://cmput404-group6-instatonne.herokuapp.com', 'https://*', 'http://*'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    '*'
+    'https://*.t20-social-distribution.herokuapp.com',
+    'http://*.127.0.0.1:8000','http://*.localhost:8000','http://*.localhost:8001','http://*.localhost:3000',
+    'https://*.epic-app.vercel.app',
+    'https://*.group-13-epic-app.herokuapp.com', 'https://*.social-distribution-media.herokuapp.com',
+    'https://*.cmput404-group6-instatonne.herokuapp.com', 'https://*', 'http://*'
 ]
 
 ROOT_URLCONF = 'backend.urls'
